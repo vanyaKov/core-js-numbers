e3c10f3c65337bc0a91e316e28e9cf9138128aae
vanyaKov <85314590+vanyaKov@users.noreply.github.com>
2024-11-09 14:26:01 +0300

    docs: completed task

--- src/numbers-tasks.js
+++ src/numbers-tasks.js
@@ -131,7 +131,7 @@ function getAngleBetweenVectors(x1, y1, x2, y2) {
  *     0     => 0
  */
 function getLastDigit(value) {
-  return String(value)[String(value).length - 1];
+  return Math.abs(value) % 10;
 }
 
 /**
